045c1e721c3fedc101af572a5bef608ab559447d
Jejuuuuu <troyantonio32@gmail.com>
2024-05-14 09:36:15 +0800

    added index

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ if (isset($_POST['delete'])) {
          ?>  
 
         <tr>
-          <td><?php echo $counter;?> </td>
+          <td><?php echo $counter++;?> </td>
           <td><?php echo $row['firstname'];?></td>
           <td><?php echo $row['lastname'];?></td>
           <td><?php echo $row['birthday'];?></td>
